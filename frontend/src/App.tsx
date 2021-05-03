c86aee3795ede20c7b84e7b7bf4371c7163e420b
Natan Ribeiro <ntnmarcal@gmail.com>
2021-05-03 17:58:35 -0300

    add basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import AppBar from 'components/appbar';
+import DataTable from 'components/datatable';
+import Footer from 'components/footer';
 import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <AppBar></AppBar>
+    <div className="App container">
       <h1 className='text-primary'>Olá Mundo!</h1>
+      <DataTable></DataTable>
     </div>
+    <Footer></Footer>
+    </>
   );
 }
 
